9613454b88db2ab963bad5e52e71e1cb1f8b7025
Max Braynner <max.braynner@gmail.com>
2019-03-13 22:15:27 -0300

    saving on mongodb

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
+require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
 const graphqlHttp = require("express-graphql");
 const { buildSchema } = require("graphql");
+const mongoose = require("mongoose");
+
+const Event = require("./models/event");
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-
-const events =  [];
 
 app.use(
     "/graphql",
@@ -43,22 +46,41 @@ app.use(
             }
         `),
         rootValue: {
-            events: () => events,
-            createEvent: (args) => {
-                const eventInput = args.eventInput;
-                const event = {
-                    _id: Math.random().toString(),
+            events: async () => {
+                return await Event.find();
+            },
+            createEvent: async args => {
+                const { eventInput } = args;
+                const event = new Event({
                     title: eventInput.title,
                     description: eventInput.description,
                     price: eventInput.price,
-                    date: eventInput.date,
+                    date: new Date(eventInput.date)
+                });
+
+                try {
+                    return await event.save();
+                } catch (error) {
+                    console.log(error);
+                    throw error;
                 }
-                events.push(event);
-                return event;
             }
         },
         graphiql: true
     })
 );
 
-app.listen(process.env.PORT || 4000);
+(async () => {
+    try {
+        const { PORT } = process.env;
+
+        await mongoose.connect(process.env.MONGO_URL, {
+            useNewUrlParser: true
+        });
+        await app.listen(PORT || 4000);
+
+        console.log(`Listening on port ${PORT}`);
+    } catch (error) {
+        console.error(err);
+    }
+})();
