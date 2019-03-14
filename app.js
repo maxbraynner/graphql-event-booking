e3cbd2be8634dc92f29a3f4ad7212097b20fccb9
Max Braynner <max.braynner@gmail.com>
2019-03-13 23:27:53 -0300

    Adding relations

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ const cors = require("cors");
 const graphqlHttp = require("express-graphql");
 const { buildSchema } = require("graphql");
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const Event = require("./models/event");
+const User = require("./models/user");
 
 const app = express();
 app.use(express.json());
@@ -25,11 +27,22 @@ app.use(
                 date: String!
             }
 
+            type User {
+                _id: ID!
+                email: String!
+                password: String
+            }
+
             input EventInput {
                 title: String!
                 description: String!
                 price: Float!
                 date: String!
+            }
+
+            input UserInput {
+                email: String!
+                password: String!
             }
 
             type RootQuery {
@@ -38,6 +51,7 @@ app.use(
 
             type RootMutation {
                 createEvent(eventInput: EventInput): Event
+                createUser(userInput: UserInput): User
             }
 
             schema {
@@ -50,16 +64,56 @@ app.use(
                 return await Event.find();
             },
             createEvent: async args => {
-                const { eventInput } = args;
-                const event = new Event({
-                    title: eventInput.title,
-                    description: eventInput.description,
-                    price: eventInput.price,
-                    date: new Date(eventInput.date)
-                });
+                const userId = "5c89b4604562c03d55d8bb67";
 
                 try {
-                    return await event.save();
+                    const { eventInput } = args;
+
+                    const user = await User.findById(userId);
+
+                    if (!user) {
+                        throw new Error("User do not exists");
+                    }
+
+                    const event = new Event({
+                        title: eventInput.title,
+                        description: eventInput.description,
+                        price: eventInput.price,
+                        date: new Date(eventInput.date),
+                        creator: user._id
+                    });
+
+                    const result = await event.save();
+
+                    user.createdEvents.push(event);
+                    await user.save();
+
+                    return result;
+                } catch (error) {
+                    console.log(error);
+                    throw error;
+                }
+            },
+            createUser: async args => {
+                try {
+                    const { userInput } = args;
+
+                    const hasUser = await User.findOne({
+                        email: userInput.email
+                    });
+
+                    if (hasUser) {
+                        throw new Error("User exists already!");
+                    }
+
+                    const hash = await bcrypt.hash(userInput.password, 8);
+                    const user = new User({
+                        email: userInput.email,
+                        password: hash
+                    });
+
+                    const result = await user.save();
+                    return { email: result.email, _id: result._id };
                 } catch (error) {
                     console.log(error);
                     throw error;
